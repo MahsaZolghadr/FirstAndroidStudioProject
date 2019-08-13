7fee69bbf134c88d5abf00515f57b0e94e5c02aa
LENOVO@DESKTOP-ISOUGI7 <mahsa.er@gmail.com>
2019-08-13 12:02:33 +0430

    New Changes

--- app/src/main/java/com/firstandroidstudioproject/Mordad98/MainActivity.java
+++ app/src/main/java/com/firstandroidstudioproject/Mordad98/MainActivity.java
@@ -3,6 +3,7 @@ package com.firstandroidstudioproject.Mordad98;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       Log.d("MYTAG","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("MYTAG","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("MYTAG","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("MYTAG","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("MYTAG","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("MYTAG","onDestroy");
     }
 }
